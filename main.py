f7e0abffff36eb161941f7645180fa68d9839cc6
qvant <qvant86@gmail.com>
2016-04-21 23:28:46 +0300

    Added dungeon generation

--- main.py
+++ main.py
@@ -7,6 +7,46 @@ LIMIT_FPS = 20
 MAP_WIDTH = 80
 MAP_HEIGHT = 45
 
+ROOM_MAX_SIZE = 10
+ROOM_MIN_SIZE = 6
+MAX_ROOMS = 30
+
+class Rect:
+	def __init__(self, x, y, w, h):
+		self.x1 = x
+		self.x2 = x + w
+		self.y1 = y
+		self.y2 = y + h
+	def center(self):
+		center_x = (self.x1 + self.x2) / 2
+		center_y = (self.y1 + self.y2) / 2
+		return (center_x, center_y)
+	def intersect(self, other):
+		# return true if rectanges intersected
+		return (self.x1 <= other.x2 and self.x2 >= other.x1 and
+                self.y1 <= other.y2 and self.y2 >= other.y1)
+
+def create_room(room):
+	global map
+	#go through the tiles in the rectangle and make them passable
+	for x in range(room.x1 + 1, room.x2):
+		for y in range(room.y1 + 1, room.y2):
+			map[x][y].blocked = False
+			map[x][y].block_sight = False
+			
+def create_h_tunnel(x1, x2, y):
+	global map
+	for x in range(min(x1, x2), max(x1, x2) + 1):
+		map[x][y].blocked = False
+		map[x][y].block_sight = False
+		
+def create_v_tunnel(y1, y2, x):
+	global map
+	#vertical tunnel
+	for y in range(min(y1, y2), max(y1, y2) + 1):
+		map[x][y].blocked = False
+		map[x][y].block_sight = False
+
 def handle_keys():
 	global playerx, playery
 	
@@ -73,13 +113,65 @@ def make_map():
 	global map
 	
 	#fill map with "unblocked" tiles
-	map = [[Tile(False)
+	map = [[Tile(True)
 		for y in range(MAP_HEIGHT)]
 			for i in range(MAP_WIDTH)]
-	map[30][22].blocked = True
-	map[30][22].block_sight = True
-	map[50][22].blocked = True
-	map[50][22].block_sight = True
+	#room1 = Rect(20, 15, 10, 15)
+	#room2 = Rect(50, 15, 10, 15)
+	#create_room(room1)
+	#create_room(room2)
+	#create_h_tunnel(25, 55, 23)
+	rooms = []
+	num_rooms = 0
+	for i in range(MAX_ROOMS):
+		width = libtcod.random_get_int(0, ROOM_MIN_SIZE, ROOM_MAX_SIZE)
+		height = libtcod.random_get_int(0, ROOM_MIN_SIZE, ROOM_MAX_SIZE)
+		x = libtcod.random_get_int(0, 0, MAP_WIDTH - width - 1 )
+		y = libtcod.random_get_int(0, 0, MAP_HEIGHT - height - 1 )
+		
+		#"Rect" class makes rectangles easier to work with
+		new_room = Rect(x, y, width, height)
+		#run through the other rooms and see if they intersect with this one
+		failed = False
+		for other_room in rooms:
+			if new_room.intersect(other_room):
+				failed = True
+				break
+		if not failed:
+			#this means there are no intersections, so this room is valid
+			
+			#"paint" it to the map's tiles
+			create_room(new_room)
+			
+			#center coordinates of new room, will be useful later
+			(new_x, new_y) = new_room.center()
+			
+			if num_rooms == 0:
+				#this is the first room, where the player starts at
+				player.x = new_x
+				player.y = new_y
+			else:
+				#all rooms after the first:
+				#connect it to the previous room with a tunnel
+				
+				#center coordinates of previous room
+				(prev_x, prev_y) = rooms[num_rooms-1].center()
+				
+				#draw a coin (random number that is either 0 or 1)
+				if libtcod.random_get_int(0, 0, 1) == 1:
+					#first move horizontally, then vertically
+					create_h_tunnel(prev_x, new_x, prev_y)
+					create_v_tunnel(prev_y, new_y, new_x)
+				else:
+					#first move vertically, then horizontally
+					create_v_tunnel(prev_y, new_y, prev_x)
+					create_h_tunnel(prev_x, new_x, new_y)
+ 
+			#finally, append the new room to the list
+			rooms.append(new_room)
+			num_rooms += 1
+			room_no = Object(new_x, new_y, chr(65+num_rooms), libtcod.white)
+			objects.insert(0, room_no) #draw early, so monsters are drawn on top
 	
 def render_all():
 	global color_light_wall
@@ -115,6 +207,8 @@ playerx = SCREEN_WIDTH/2
 playery = SCREEN_HEIGHT/2
 
 player = Object(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, '@', libtcod.white)
+player.x = 25
+player.y = 23
 npc = Object(SCREEN_WIDTH/2 - 5, SCREEN_HEIGHT/2, '@', libtcod.yellow)
 objects = [npc, player]
 
